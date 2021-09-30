78529282838448ef1eca4bbe6e599339f4de4bd4
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-09-30 12:49:35 +0530

    spring

---
+++ spring.io/src/main/java/org/java/full/stack/spring/io/AsyncWebClient.java
@@ -0,0 +1,31 @@
+package org.java.full.stack.spring.io;
+
+import java.util.List;
+
+import org.java.full.stack.spring.io.model.LocalShopProduct;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class AsyncWebClient {
+	private final WebClient client;
+	private final String baseUrl = "http://localhost:8080";
+
+	public AsyncWebClient(WebClient.Builder webClientBuilder) {
+		this.client = webClientBuilder.baseUrl(baseUrl).build();
+	}
+
+	public Mono<LocalShopProduct> findByName(String name) {
+		return this.client.get().uri("/name/{name}", name)
+				.retrieve().bodyToMono(LocalShopProduct.class);
+	}
+
+	public Flux<LocalShopProduct> findAll() {
+		Flux<LocalShopProduct> all = this.client.get().uri("/products").retrieve()
+				.bodyToFlux(LocalShopProduct.class);
+		return all;
+	}
+}
